763bd894763548f27dd9d6823383734a3c117d46
WaritsaraW <Waritsara.Watcharapong@student.uts.edu.au>
2025-05-18 16:04:04 +1000

    Update ecosystem.config.js to modify script path and adjust start arguments for improved deployment configuration.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,17 +2,10 @@ module.exports = {
   apps: [
     {
       name: 'car-rental-system',
-      script: 'node_modules/next/dist/bin/next',
-      args: 'start',
-      instances: 'max',
-      exec_mode: 'cluster',
-      autorestart: true,
+      script: './node_modules/next/dist/bin/next',
+      args: 'start -p '+(process.env.PORT || 3000),
       watch: false,
-      max_memory_restart: '1G',
-      env: {
-        NODE_ENV: 'production',
-        PORT: 8080
-      }
+      autorestart: true
     }
   ]
 }; 
