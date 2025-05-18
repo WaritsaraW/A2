ab1d1b3f39a4149f884b90a89febda0efa5b19da
WaritsaraW <Waritsara.Watcharapong@student.uts.edu.au>
2025-05-18 17:02:29 +1000

    Refactor ecosystem.config.js to change the application start script to Next.js and adjust the start command to include dynamic port configuration, while maintaining autorestart settings.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,19 +2,10 @@ module.exports = {
   apps: [
     {
       name: 'car-rental-system',
-      script: 'npm',
-      args: 'start',
-      instances: 'max',
-      exec_mode: 'cluster',
-      autorestart: true,
+      script: './node_modules/next/dist/bin/next',
+      args: 'start -p '+(process.env.PORT || 8080),
       watch: false,
-      max_memory_restart: '1G',
-      env: {
-        NODE_ENV: 'production',
-        PORT: 8080
-      },
-      // Run build command before starting the application
-      setup: 'npm run build'
+      autorestart: true
     }
   ]
 }; 
